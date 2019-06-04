eeb550919d9452bd71352e4aee24e301e64aecd4
Koalla <pedrolucca257@gmail.com>
2019-06-04 00:11:20 -0300

    Crud 1.0

---
+++ Entrevista/Entrevista/Models/PessoaContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Entrevista.Models
+{
+    public class PessoaContext : DbContext
+    {
+        public PessoaContext() 
+            : base("Entrevista")
+        {
+
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+       
+    }
+}
